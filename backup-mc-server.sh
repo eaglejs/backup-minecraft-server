387de25b6cfa50daf862c2f0a9c53a21e24ec0c8
eaglejs <joshuaseagle@gmail.com>
2019-09-19 20:49:38 -0400

    removed code

--- backup-mc-server.sh
+++ backup-mc-server.sh
@@ -1,22 +1,3 @@
 #!/bin/bash
 
 npm run prod
-
-
-
-
-
-
-# date=$(date +%Y%m%d:%H%M%S)
-# pattern='/\:([0-9])*/'
-# match=''
-# files=($(find *.tar.gz))
-
-# # tar -czvf mcs-$date.tar.gz /Users/jeagle/Downloads/minecraft-server
-
-# for file in $files; do
-# 	# if [[ $file =~ $pattern ]]; then
-# 	# echo "${file%.tar.gz}"
-# 	[[ "a b"  =~  ^a\ b$ ]] && echo yes || echo no
-# 	# fi
-# done
